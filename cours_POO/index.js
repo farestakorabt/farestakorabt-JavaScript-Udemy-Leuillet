1de5f72b6a53cb5578c25a285c2e1a14d845706c
fares takorabt <farestakorabt@gmail.com>
2022-01-27 22:50:37 +0100

    POO end

--- cours_POO/index.js
+++ cours_POO/index.js
@@ -97,14 +97,14 @@ function Oiseau(nombreDePattes, poids, longueurDesAiles) {
   // arguments de son constructeur
   this.longueurDesAiles = longueurDesAiles;
 }
-Oiseau.prototype = Object.create(Animal.prototype);
-Oiseau.prototype.constructor = Oiseau;
 
 // Mammifere (nombreDePattes, poids / typeDePoils)
 function Mammifere(nombreDePattes, poids, typeDePoils) {
   Animal.call(this, nombreDePattes, poids);
   this.typeDePoils = typeDePoils;
 }
+Oiseau.prototype = Object.create(Animal.prototype);
+Oiseau.prototype.constructor = Oiseau;
 
 var perroquet = new Oiseau(2, "11kg", "grandes");
 // perroquet.presentation();
@@ -119,9 +119,114 @@ var chat = new Mammifere(4, "7kg", "court");
 // exemple: Animal.call(this, nombreDePattes, poids);
 
 let gandalf = { titre: "Magicien" };
-
-function direBonjour() {
-  console.log("Bonjour " + this.titre);
+function direBonjour(arme, degats) {
+  // console.log(
+  //   "Bonjour " +
+  //     this.titre +
+  //     ", vous possedez " +
+  //     arme +
+  //     " qui donne " +
+  //     degats +
+  //     " points de degat."
+  // );
 }
 
-direBonjour();
+// direBonjour.apply(gandalf, ["un baton", 75]); // same result => apply needs array[] => lot of arguments like JSON file
+direBonjour.call(gandalf, "un baton", 75); // same result => call needs arguments separated => few arguments
+
+// Bind
+
+// objectif est de changer le context de "this"
+
+// this tout seul (en dehors d'une fonction vaut window)
+
+this.valeur = "window"; // this vaut window sur cet exemple
+
+let monObjet2 = {
+  valeur: "maChose",
+  getValeur: function () {
+    console.log(this.valeur);
+  },
+};
+
+// monObjet2.getValeur();
+
+let maValeur = monObjet.getValeur;
+
+let mavaleurAvecBind = monObjet2.getValeur.bind(monObjet2);
+// mavaleurAvecBind();
+
+// ----------------------------
+// Créer un objet avec une class
+// ----------------------------
+
+class Utilisateur2 {
+  constructor(prenom, nom, email) {
+    this.prenom = prenom;
+    this.nom = nom;
+    this.email = email;
+  }
+
+  // un getter (accesseur) permet de récupérer une propriété
+  get nomComplet() {
+    return this.prenom + " " + this.nom;
+  }
+
+  // un setter (mutateur) permet de définir une propriété
+  set nomComplet(valeur) {
+    [this.prenom, this.nom] = valeur.split(' '); 
+  }
+
+  sePresenterBis() {
+    console.log(
+      "Bonjour, je m'appelle " +
+        this.prenom +
+        " " +
+        this.nom +
+        " et vous pouvez me contacter sur mon email: " +
+        this.email
+    );
+  }
+}
+var monPote = new Utilisateur2("Eric", "Cartman", "cartmanland@hello.dv");
+console.log(monPote.nomComplet);
+
+monPote.nomComplet = "Bill Murray";
+console.log(monPote.nomComplet)
+
+// class Animal2 {
+//   constructor(nombreDePattes2, poids2) {
+//     this.nombreDePattes2 = nombreDePattes2;
+//     this.poids2 = poids2;
+//   }
+
+//   animPresent() {
+//     console.log(
+//       "Cet animal possède " +
+//         this.nombreDePattes2 +
+//         " pattes et pèse " +
+//         this.poids2
+//     );
+//   }
+// }
+
+// class Oiseau2 extends Animal2 {
+//   constructor(nombreDePattes2, poids2, longueurDesAiles2) {
+//     super(nombreDePattes2, poids2);
+//     this.longueurDesAiles2 = longueurDesAiles2;
+//   }
+
+//   voler() {
+//     console.log("L'oiseau vol !");
+//   }
+// }
+
+// var crow = new Oiseau2(4, "11kg", "longues");
+// // console.log(crow);
+
+// crow.voler(); // L'oiseau vol !
+// crow.animPresent(); // Cet animal possède 4 pattes et pèse 11kg
+
+// ----------------------------
+// les Getter et Setter
+// ----------------------------
